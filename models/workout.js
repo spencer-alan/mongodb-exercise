c0c02b28439de8d3d5fbc342be3526529b82ab4b
b1acksmyth <smith.spence13@gmail.com>
2020-10-16 15:22:45 -0700

    updated model

--- models/workout.js
+++ models/workout.js
@@ -8,32 +8,7 @@ const workoutSchema = new Schema({
     default: Date.now,
     unique: true
   },
-  exercises: [
-    {
-      type: {
-        type: String,
-        unique: false
-      },
-      name: {
-        type: String
-      },
-      duration: {
-        type: Number
-      },
-      distance: {
-        type: Number
-      },
-      weight: {
-        type: Number
-      },
-      reps: {
-        type: Number
-      },
-      sets: {
-        type: Number
-      }
-    }
-  ]
+  exercises: []
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
