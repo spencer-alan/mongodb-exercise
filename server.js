e6d2f735f75478c34f943fe2b68b8c56a84d8bfd
b1acksmyth <smith.spence13@gmail.com>
2020-10-16 14:18:27 -0700

    server updated to run on localhost

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000
 
 const app = express();
 
+app.use(morgan("combined"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
 });
 
 // routes
-app.use(require("./routes/api.js"));
+require("./routes/api.js")(app);
+require("./routes/html.js")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
